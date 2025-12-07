e2cad9c7395c4695fa6fbc207524a90fc813da67
Nazih Ouchta <75317893+Yvesei@users.noreply.github.com>
2025-12-07 04:05:54 +0100

    [backend] allow cross origin comm

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import propertiesRoutes from "./routes/properties.routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3900",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.use(express.json());
 
